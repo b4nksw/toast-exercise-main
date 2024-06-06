533c3fb8184db388027c21ac8e84648f9970ca5b
Ben Wong <benksw@Bens-MacBook-Pro.local>
2024-06-06 10:04:55 -0700

    test(app): add new test case for submission button

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import App from './App';
 
-test('renders header text', () => {
-  render(<App />);
+describe('App Component', () => {
+  test('renders header text', () => {
+    render(<App />);
 
-  const heading  = screen.getByRole('heading', { name: /toast exercise/i});
-  expect(heading).toBeInTheDocument();
-});
+    const heading  = screen.getByRole('heading', { name: /toast exercise/i});
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('render new submission button', () => {
+    render(<App />);
+
+    const newSubmissionButton = screen.getByRole('button', { name: /new submission/i});
+    expect(newSubmissionButton).toBeInTheDocument();
+  })
+})
